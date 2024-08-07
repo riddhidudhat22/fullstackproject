85e4dbbec7aa03fdc3addbee3e21c8cb7c778822
riddhidudhat22 <ridhdhidudhat2003@gmail.com>
2024-08-07 10:03:07 +0530

    7-8

---
+++ backend/ecomerce/src/middleware/validation.js
@@ -0,0 +1,79 @@
+const Joi = require("joi");
+const pick = require("../helper/helper");
+
+const validation = (schema) => (req, res, next) => {
+    console.log(schema);
+    console.log(Object.keys(schema));
+
+    const obj = pick(req, Object.keys(schema))
+    console.log('ax',obj);
+
+    const { error, value } = Joi.compile(schema)
+        .prefs({
+            abortEarly: false
+        })
+        .validate(obj)
+
+
+    if (error) {
+        const errormsg = error.details.map((v) => v.message).Join(", ")
+        console.log(errormsg);
+        return next(new Error("validation Error.."+errormsg))
+   
+      
+
+    }
+    Object.assign(req,value)
+
+    next();
+}
+
+
+module.exports = {
+    validation
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Joi = require("joi");
+// const pick = require("../helper/helper");
+
+// const validation = (schema) => (req, res, next) => {
+//     console.log(schema);
+//     console.log(Object.keys(schema));
+
+//     const obj = pick(req, Object.keys(schema));
+//     console.log(obj);
+
+//     const { error, value } = Joi.compile(schema)
+//         .prefs({
+//             abortEarly: false
+//         })
+//         .validate(obj);
+
+//     if (error) {
+//         const errormsg = error.details.map((v) => v.message).join(", ");
+//         return res.status(400).json({ error: errormsg });
+//     }
+
+//     next();
+// };
+
+// module.exports = {
+//     validation
+// };
